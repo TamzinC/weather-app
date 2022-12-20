fe0ba8b5c11376adf10fa59a590149cc8b5fffec
TamzinC <115940593+TamzinC@users.noreply.github.com>
2022-12-20 23:06:57 +0000

    adding minor updates - basic css styling and psuedo code to js file to get started

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
 
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// NEED TO USE THIS TO ADD ICON TO REPRESENT WEATHER!! https://openweathermap.org/img/w/ + ICON_ID + .png
+
+/* 
+  When Page Loads:
+
+  1. Show user an input to allow them to search for a city
+    - show a message on the page to point them, or guide them, to the input.
+    - Once city has been inputted:
+      a. Show Current Forecast
+      b. Show 5 day Forecast
+      c. Add city name to search history
+        - Get previous searches from localStorage
+        - If inputted city has not been stored to search history in localStorage, push the city name
+        - Set the search history to localStorage
+  2. Show search history
+    - Pull search history from localStorage
+    - If search history is not empty, output each city to the search history display in the DOM
+*/
+
+//Add event listener for input (show weather + add to search history if not already there)
+
+
+
 
 var apiKey = '903bdd38e14db35f1d502c3f3db85a20';
 var city = 'London'; //this needs to be changed to user input
@@ -28,8 +51,7 @@ $.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}
     });
 
 
-    //NEED TO USE THIS TO ADD ICON TO REPRESENT WEATHER!!
-    //https://openweathermap.org/img/w/ + ICON_ID + .png
+   
 
 
     //Getting users current location:
