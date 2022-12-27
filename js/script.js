1476f3a477bebd8a38435ef45d4a4f7b857893fc
TamzinC <115940593+TamzinC@users.noreply.github.com>
2022-12-27 20:46:37 +0000

    updated code to add event listeners and updated some css

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var searchButton = $('#search-button');
 var currentWeather = $('#today');
 var forecastWeather = $('#forecast');
 var searchHistory = $('#history');
+var locationHistory = $('.location-history');
 var apiKey = '903bdd38e14db35f1d502c3f3db85a20';
 
 
@@ -16,12 +17,16 @@ function displayCurrentWeather(currentData) {
     console.log(currentDay);
 
     currentWeather.append(`
-    <h1>${currentData.name} (${currentDay}) <img src="https://openweathermap.org/img/w/${weatherIcon.icon}.png" alt="${weatherIcon.description}"/></h1>
+    <div class="mt-3 jumbotron jumbotron-fluid">
+    <div class="container">
+    <h2>${currentData.name} (${currentDay}) <img src="https://openweathermap.org/img/w/${weatherIcon.icon}.png" alt="${weatherIcon.description}"/></h1>
     <ul class="current-weather row">
     <li>Temp: ${Math.round(currentData.main.temp)} °C</li> 
     <li>Wind: ${currentData.wind.speed} M/S</li>
     <li>Humidity: ${currentData.main.humidity}%</li>
     </ul>
+    </div>
+    </div>
     `);
 
 }
@@ -40,7 +45,7 @@ function displayForecastWeather(forecastData) {
         var forecast = forecastDays[index];
         var weatherIcon = forecast.weather[0];
         output += `<li class="shadow-lg p-3 rb-5 bg-white rounded">
-        <p>${forecast.dt_txt}</p>
+        <p>${moment().add(1, 'days').format('DD/MM/YYYY')}</p>
         <img src="https://openweathermap.org/img/w/${weatherIcon.icon}.png" alt="${weatherIcon.description}"/>
         <p>Temp: ${Math.round(forecast.main.temp)} °C</p>
         <p>Wind: ${forecast.wind.speed} M/S</p>
@@ -50,46 +55,68 @@ function displayForecastWeather(forecastData) {
 
     forecastWeather.append(`
         <h3>5-Day Forecast:</h1>
-        <ul class="current-forecast">${output}</ul>
+        <ul class="future-forecast">${output}</ul>
         `)
 };
+
+//<p>${forecast.dt_txt}</p>
 
 //filtering the forecast weather data to only show data for the next 5 days instead of every 3hrs
 function filterByDateTime(forecastDate) {
     return forecastDate.dt_txt.indexOf('12:00:00') > 0;
 };
 
-//Setting localStorage to save user search history and display below submit button
-function addToSearchHistory () {
-    localStorage.setItem('location', JSON.stringify(searchInput));
+// Setting localStorage to save user search history and display below submit button
+function addToSearchHistory() {
+    localStorage.setItem('location', JSON.stringify(searchInput.val()));
 
     var location = JSON.parse(localStorage.getItem('location'));
-    searchHistory.append (`
-    <p>${location}</p>
+    searchHistory.append(`
+    <button data-location="${location}" type="button" class="location-history btn btn-secondary btn-block m-1">${location}</button>
     `);
+    
+    locationHistory.on('click', function() {
+        searchInput = $(location);
+        getWeather();
+    })
 }
+
+// function addToSearchHistory () {
+//     localStorage.setItem('location', JSON.stringify(searchInput.val()));
+
+//     var location = JSON.parse(localStorage.getItem('location'));
+//     searchHistory.append (`
+//         <p data-location="${location}">${location}</p>
+//     `);
+
+//     $('#history p').click(function (event) {
+//         console.log($(this).data('location')); // https://www.w3schools.com/js/js_this.asp
+//         console.log(event);
+//     });
+// }
 
 
 //Creating API requests to get current and forecasted weather
 function getWeather(event) {
     event.preventDefault();
-
+    
     //API request for getting current weather info for a location
     $.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchInput.val()}&appid=${apiKey}&units=metric`)
-        .then(function (currentData) {
-            var lon = currentData.coord.lon;
-            var lat = currentData.coord.lat;
-            displayCurrentWeather(currentData);
-
-            //API request for getting forecasted weather for a location
-            $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
-                .then(function (forecastData) {
-                    displayForecastWeather(forecastData);
-                });
-
-            searchInput.val('');
-            addToSearchHistory();
-            
+    .then(function (currentData) {
+        var lon = currentData.coord.lon;
+        var lat = currentData.coord.lat;
+        displayCurrentWeather(currentData);
+        
+        //API request for getting forecasted weather for a location
+        $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
+        .then(function (forecastData) {
+            displayForecastWeather(forecastData);
+        });
+        
+        addToSearchHistory();
+        searchInput.val('');
+        
+        
         });
 }
 
